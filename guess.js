f3eaebbdf2e61ebafc2e73ea5442373039656b20
MD KAMRUL HASAN <114093612+chasmkhasan@users.noreply.github.com>
2022-10-25 19:36:09 +0200

    added some js code.

--- guess.js
+++ guess.js
@@ -1,42 +1,55 @@
 /* alert('test') */
-var btnSubmit = document.getElementById('btnSubmit');
-var btnPlayAgain = document.getElementById('btnPlayAgain');
-var output = document.getElementById('outputText');
-var outputList = document.getElementById('list');
+let btnSubmit = document.getElementById('btnSubmit');
+let btnPlayAgain = document.getElementById('btnPlayAgain');
+let output = document.getElementById('outputText');
+let outputList = document.getElementById('list');
 
-var randomNumber = Math.ceil(Math.random()*100) + 1;
+let randomNumber = Math.ceil(Math.random()*100) + 1;
 
 function checkNumber(){
-    var input = parseInt(document.getElementById('userInput').value);
-    if(input === randomNumber){
-        output.innerHTML="You guessed the right number "+ ", "+ " it was "+ randomNumber;
-        output.style.color = "green";
-        outputList.innerHTML += "Matches number is " + input + "<br/>";
-    }
-    else if (input > randomNumber && input <= 100) {
-        output.innerHTML="You guessed too high ";
-        output.style.color = "red";
-        outputList.innerHTML += "Too high " + input + "<br/>";
-    }
-    else if (input < randomNumber && input >= 1){
-        output.innerHTML="You guessed too low ";
-        output.style.color = "blue";
-        outputList.innerHTML += "Too low is " + input + "<br/>";
-    }
-    else if (input <= 1){
-        output.innerHTML="Inputed number is Out of range ";
-        output.style.color = "aqua";
-    }
-    else if (isNaN(input)){
-        output.innerHTML = "That's not a number ";
-    }
-    else {
-        output.innerHTML = "Something is wrong, try again ";
-    }
+    
+        let input = parseInt(document.getElementById('userInput').value);
+        if(input === randomNumber){
+            output.innerHTML="You guessed the right number "+ ", "+ " it was "+ randomNumber;
+            output.style.color = "green";
+            outputList.innerHTML += "Matches number is " + input + "<br/>";
+        }
+        else if (input > randomNumber && input <= 100) {
+            output.innerHTML="You guessed too high ";
+            output.style.color = "red";
+            outputList.innerHTML += "Too high " + input + "<br/>";
+        }
+        else if (input < randomNumber && input >= 1){
+            output.innerHTML="You guessed too low ";
+            output.style.color = "blue";
+            outputList.innerHTML += "Too low is " + input + "<br/>";
+        }
+        else if (input <= 1){
+            output.innerHTML="Inputed number is Out of range ";
+            output.style.color = "aqua";
+        }
+        else if (isNaN(input)){
+            output.innerHTML = "That's not a number ";
+        }
+        else {
+            output.innerHTML = "Something is wrong, try again ";
+        }
+
     
 }
 btnSubmit.addEventListener("click",checkNumber);
 btnPlayAgain.addEventListener("click", function(){
     location.reload();
 });
+
+/* function clickCounter() {
+    if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+        localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "You guessed " + localStorage.clickcount + " time(s).";
+    }
+} */
 
